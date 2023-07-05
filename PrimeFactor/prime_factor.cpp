3df4245cab7719d63902ab2b47c7d733e215c2be
dioprg <dioprg0@gmail.com>
2023-07-05 15:27:03 +0900

    [refactoring] Of 4, 6, 9 commit

--- PrimeFactor/prime_factor.cpp
+++ PrimeFactor/prime_factor.cpp
@@ -10,24 +10,16 @@ public:
 		if (number > 1)
 		{
 			int divisor = 2;
-			if (number == 4)
-			{
-				while (number % divisor == 0)
-				{
-					ret.push_back(divisor);
-					number /= divisor;
-				}
-			} else if (number == 6)
+			if (number == 4 || number == 6 || number == 9)
 			{
 				for (divisor = 2; number > 1; divisor++)
 				{
-					ret.push_back(divisor);
-					number /= divisor;
+					while (number % divisor == 0)
+					{
+						ret.push_back(divisor);
+						number /= divisor;
+					}
 				}
-			} else if (number == 9)
-			{
-				ret.push_back(3);
-				ret.push_back(3);
 			}
 			else
 			{
