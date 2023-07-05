1cce191388e41c2f0b8450af58c0427f33d9fb57
dioprg <dioprg0@gmail.com>
2023-07-05 15:31:14 +0900

    [refactoring] Of method

--- PrimeFactor/prime_factor.cpp
+++ PrimeFactor/prime_factor.cpp
@@ -9,27 +9,15 @@ public:
 		vector<int> ret = {};
 		if (number > 1)
 		{
-			int divisor = 2;
-			if (number == 4 || number == 6 || number == 9)
+			for (int divisor = 2; number > 1; divisor++)
 			{
-				for (divisor = 2; number > 1; divisor++)
+				while (number % divisor == 0)
 				{
-					while (number % divisor == 0)
-					{
-						ret.push_back(divisor);
-						number /= divisor;
-					}
+					ret.push_back(divisor);
+					number /= divisor;
 				}
-			} else if (number == 12)
-			{
-				ret.push_back(2);
-				ret.push_back(2);
-				ret.push_back(3);
 			}
-			else
-			{
-				ret.push_back(number);
-			}
+			
 		}
 		return ret;
 	}
