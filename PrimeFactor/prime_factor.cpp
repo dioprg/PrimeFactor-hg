0e6db73ad8304c7f438fe11ea34a8b1e9df277a8
dioprg <dioprg0@gmail.com>
2023-07-05 15:11:48 +0900

    Of 2, 3 commit

--- PrimeFactor/prime_factor.cpp
+++ PrimeFactor/prime_factor.cpp
@@ -7,6 +7,14 @@ public:
 	vector<int> of(int i)
 	{
 		vector<int> ret = {};
-		if (i <= 1) return ret;
+		if (i == 2)
+		{
+			ret.push_back(2);
+		}
+		if (i == 3)
+		{
+			ret.push_back(3);
+		}
+		return ret;
 	}
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -4,8 +4,25 @@
 #include <vector>
 using namespace std;
 
-TEST(PrimeFactors, Of1) {
+class PrimeFixture : public testing::Test
+{
+public:
 	PrimeFactor prime_factor;
-	vector<int> expected = {};
+	vector<int> expected;
+};
+
+
+TEST_F(PrimeFixture, Of1) {
+	expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
+}
+
+TEST_F(PrimeFixture, Of2) {
+	expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
+}
+
+TEST_F(PrimeFixture, Of3) {
+	expected = {3};
+	EXPECT_EQ(expected, prime_factor.of(3));
 }
